0afa716cf2af88ceb941f6204f59029a5245fee8
Lesly Lopez <leslylpz32@gmail.com>
2024-04-24 11:04:12 -0500

    completed tips.js

---
+++ tips.js
@@ -0,0 +1,10 @@
+//variables create as inputs for calculation
+let foodCost = 79.29;
+let tax = 6.54;
+let tip = 12.00;
+
+//do calculations for variables above
+let totalDue = foodCost + tax + tip;
+//output the results to the connsole
+
+console.log ("the total due is " + totalDue);
