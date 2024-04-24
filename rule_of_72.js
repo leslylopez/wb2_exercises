1b62e35d6565839643d71b23a1a2cbfec0487667
Lesly Lopez <leslylpz32@gmail.com>
2024-04-24 13:41:40 -0500

    completed rule_of_72.js

---
+++ rule_of_72.js
@@ -0,0 +1,13 @@
+//input
+
+let interestRatePercent = 6;
+let savings = 500;
+
+//calculations
+
+let time = 72 / interestRatePercent;
+let totalSavings = savings * 2;
+
+//output
+
+console.log ("At a " + interestRatePercent, "% interest rate ", "your savings account will be worth " + totalSavings, "in " + time, "years" );
