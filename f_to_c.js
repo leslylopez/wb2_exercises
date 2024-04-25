bd347f2e056e9cef5d8c7c336956bf26a31e2c0f
Lesly Lopez <leslylpz32@gmail.com>
2024-04-25 09:32:56 -0500

    completed f_to_c.js file

---
+++ f_to_c.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// input variables
+
+let fahrenheit = 70;
+
+//calculations
+
+let celsius = (fahrenheit-32)*5/9;
+
+//output
+
+console.log ("The celsius temperature is " + celsius);
+
