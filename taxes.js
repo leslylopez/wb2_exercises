a7defca20ca0edbfdf2a86a58bba2d4b4340950f
Lesly Lopez <leslylpz32@gmail.com>
2024-04-25 09:44:43 -0500

    completed taxes.js file

---
+++ taxes.js
@@ -0,0 +1,11 @@
+// variables for input
+
+let salary = 72000;
+
+//calculations
+
+let moneywithheld = salary * .23;
+
+//output
+
+console.log ("The amount of money withheld for taxes is "+ moneywithheld);
